343871328bc660cd31c1c8e3e49016a3b3a0ead6
Heberty Carlos <heberty@maxmail.com.br>
2020-07-06 21:25:21 -0300

    Footer

--- resources/views/pages/layouts/blocks/_footer.blade.php
+++ resources/views/pages/layouts/blocks/_footer.blade.php
@@ -1,1 +1,40 @@
-<footer></footer>
+<footer class="footer-site">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 d-flex flex-column flex-sm-row justify-content-between mb60">
+				<a href="{{ route('index') }}" class="logo-footer" title="Rits"><img src="{{ asset('images/logo.svg') }}" alt="Rits"></a>
+				<ul class="list-networks">
+					<li class="list-networks-item">
+						<a href="http://.com/" class="list-networks-item-link" title="Facebook">
+							<img src="{{ asset('images/icon-facebook.svg') }}" alt="Facebook" class="list-networks-item-link-icon">
+						</a>
+					</li>
+					<li class="list-networks-item">
+						<a href="http://.com/" class="list-networks-item-link" title="Twitter">
+							<img src="{{ asset('images/icon-twitter.svg') }}" alt="Twitter" class="list-networks-item-link-icon">
+						</a>
+					</li>
+					<li class="list-networks-item">
+						<a href="http://.com/" class="list-networks-item-link" title="Instagram">
+							<img src="{{ asset('images/icon-instagram.svg') }}" alt="Instagram" class="list-networks-item-link-icon">
+						</a>
+					</li>
+					<li class="list-networks-item">
+						<a href="http://.com/" class="list-networks-item-link" title="Linkedin">
+							<img src="{{ asset('images/icon-linkedin.svg') }}" alt="Linkedin" class="list-networks-item-link-icon">
+						</a>
+					</li>
+					<li class="list-networks-item">
+						<a href="http://.com/" class="list-networks-item-link" title="Youtube">
+							<img src="{{ asset('images/icon-youtube.svg') }}" alt="Youtube" class="list-networks-item-link-icon">
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-12 d-flex flex-column flex-sm-row justify-content-between">
+				<small class="corporate">Copyright © 2020 - Rits Tecnologia. Todos os direitos reservados. Política de Privacidade</small>
+				<a href="http://rits.com.br" class="link-rits" title="Rits" target="_blank">Rits</a>
+			</div>
+		</div>
+	</div>
+</footer>
