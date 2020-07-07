7133a0f535176778096c792c4736868f2fdee2bf
Heberty Carlos <corte_heberty@hotmail.com>
2020-07-07 01:47:53 +0000

    Ajuste de variáveis

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::group(['namespace' => 'Pages'], function() {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('home');
