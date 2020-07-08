5cd0f1bab3a1cd6d789188e08b1ab62ffd31bd27
Heberty Carlos <heberty@maxmail.com.br>
2020-07-08 14:28:00 -0300

    Ajuste de imagem

--- public/js/app.js
+++ public/js/app.js
@@ -28219,8 +28219,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Docker\local.teste-rits.com.br\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Docker\local.teste-rits.com.br\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/local.teste-rits.com.br/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/local.teste-rits.com.br/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
