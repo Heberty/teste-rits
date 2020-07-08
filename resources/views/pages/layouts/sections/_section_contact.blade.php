54ffbf97ab669e9f74d04c6e13df9cf1962177f1
Heberty Carlos <heberty@maxmail.com.br>
2020-07-07 22:09:01 -0300

    Ajuste na seção contact

--- resources/views/pages/layouts/sections/_section_contact.blade.php
+++ resources/views/pages/layouts/sections/_section_contact.blade.php
@@ -3,19 +3,19 @@
 		<div class="row contain-contact no-gutters">
 			<div class="col-12 col-lg-4">
 				<a href="" class="card-contact">
-					<div class="card-contact-icon mail"></div>
+					<span class="card-contact-icon mail"></span>
 					<strong class="card-contact-title">Alguma <br>dúvida?</strong>
 				</a>
 			</div>
 			<div class="col-12 col-lg-4">
 				<a href="" class="card-contact">
-					<div class="card-contact-icon phone"></div>
+					<span class="card-contact-icon phone"></span>
 					<strong class="card-contact-title">Ligamos <br>para você</strong>
 				</a>
 			</div>
 			<div class="col-12 col-lg-4">
 				<a href="" class="card-contact" style="border-right: 0; border-bottom: 0;">
-					<div class="card-contact-icon whatsapp"></div>
+					<span class="card-contact-icon whatsapp"></span>
 					<strong class="card-contact-title">Fale pelo <br>Whatsapp</strong>
 				</a>
 			</div>
